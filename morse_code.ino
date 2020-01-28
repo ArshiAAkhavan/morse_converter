faba8249b65b2f2aff77f7f79744df3925f8c5fd
ArshiA <letmemakenewone@gmail.com>
2020-01-28 07:09:14 +0330

    phase 1 done! the code can get a word and turn it into morse code
    note that we use '#' key as the words finish character and we use KEY_HASHTAG to do so

--- morse_code.ino
+++ morse_code.ino
@@ -1,7 +1,9 @@
 #include "MultitapKeypad.h"
 
+#define KEY_HASHTAG 35
+
 // arduino config
-byte LED=13;
+byte LED=5;
 
 
 //default values of the keypad
@@ -39,13 +41,14 @@ byte keymap[Rows][Cols][taps] = {{
 
 
 
-const short shortGap=1250;
-const short wordGap=2000;
-const short dahGap=400;
+const short shortGap=200;// between each dit &dah
+const short letterGap=1000;// between to letter
+const short dahGap=500;
 const short ditGap=100;
 short dGaps[2]={ditGap,dahGap};
 byte morseCode[37]={235,189,192,225,240,165,228,162,234,175,226,171,238,237,229,174,199,219,216,241,217,163,220,190,193,198,121,40,13,4,1,0,81,108,117,120};
-
+byte inputBuffer[1000];
+int pointer=0;
 
 void bip(byte bipbip){
   digitalWrite(LED,HIGH);
@@ -59,96 +62,67 @@ void genMorse(byte code){
   Serial.print("daram ino mikonam: ");
   Serial.println(code);
   for(int i=0;i<5;i++){
-    Serial.println("so it begins");
-    Serial.println(code%3);
-    if(code%3==2)return;
+    if(code%3==2)break;
+    Serial.print(code%3);
     bip(code%3);
     code/=3;
   }
+  delay(letterGap);
+  Serial.println();
 }
+
 
 
 MultitapKeypad kpd(22 , 24 , 26 , 28 ,
                    30 , 32 , 34 , 36 );
+Key prevKey;
 
-
-void setup()
-{
-     Serial.begin(9600);  // initializing serail monitor
-//     prevKey = kpd.getKey();
-     pinMode(LED,OUTPUT);
-     
+byte getKeyPressed(Key key){
+     if(key.character>=48+1 && key.character<=48+9){
+        byte temp=key.character-49;
+        byte row=temp/3;
+        byte col=temp%3;
+        return keymap[key.tapCounter%4][row][col];  
+     }
+     return key.character;
 }
-void loop()
-{
-     Key key = kpd.getKey();
-//     handleKey(key);
-      if(key.character!= NO_KEY)
-      genMorse(morseCode[27]);
-  delay(shortGap);
+
+
+void handleKey(Key key){
   
-}
 
-void genMorse(byte code){
-  Serial.print("daram ino mikonam: ");
-  Serial.println(code);
-  for(int i=0;i<5;i++){
-    Serial.println("so it begins");
-    Serial.println(code%3);
-    if(code%3==2)return;
-    bip(code%3);
-    code/=3;
+  if(key.tapCounter==0){
+          byte inputChar=getKeyPressed(prevKey);
+          if(inputChar>='a' && inputChar<='z')inputChar-='a';
+          else if(inputChar>='0' && inputChar<='9')inputChar+=26-'0';
+          inputBuffer[pointer++]=inputChar;    
+          Serial.print(getKeyPressed(prevKey));
+          Serial.print(" ");
+          Serial.println(prevKey.tapCounter);
   }
+  if(key.character==KEY_HASHTAG){
+          Serial.println("\nmorse:");
+          for(int i=1;i<pointer;i++,Serial.print(" "))Serial.print(inputBuffer[i]);Serial.println();
+          for(int i=1;i<pointer;i++)genMorse(morseCode[inputBuffer[i]]);
+          pointer=0;  
+  }
+  
+  prevKey=key;
 }
-
-
-MultitapKeypad kpd(22 , 24 , 26 , 28 ,
-                   30 , 32 , 34 , 36 );
 
 
 void setup()
 {
      Serial.begin(9600);  // initializing serail monitor
-//     prevKey = kpd.getKey();
+     prevKey = kpd.getKey();
      pinMode(LED,OUTPUT);
      
 }
 void loop()
 {
      Key key = kpd.getKey();
-//     handleKey(key);
-      if(key.character!= NO_KEY)
-      genMorse(morseCode[27t]);
-}
-
-void genMorse(byte code){
-  Serial.print("daram ino mikonam: ");
-  Serial.println(code);
-  for(int i=0;i<5;i++){
-    Serial.println("so it begins");
-    Serial.println(code%3);
-    if(code%3==2)return;
-    bip(code%3);
-    code/=3;
-  }
-}
-
-
-MultitapKeypad kpd(22 , 24 , 26 , 28 ,
-                   30 , 32 , 34 , 36 );
-
-
-void setup()
-{
-     Serial.begin(9600);  // initializing serail monitor
-//     prevKey = kpd.getKey();
-     pinMode(LED,OUTPUT);
-     
-}
-void loop()
-{
-     Key key = kpd.getKey();
-//     handleKey(key);
-      if(key.character!= NO_KEY)
-      genMorse(morseCode[27t]);
+     if (key.code != NO_KEY)
+     { 
+        handleKey(key);
+     }
 }
